9bc90e82bec04a37ae032c367faa67280ddb8e83
Mapi <mapitenar@hotmail.com>
2024-08-07 11:47:29 +0200

    Se controla que el player ya no pueda salirse de la pantalla

--- core/src/com/svalero/stellarclash/domain/Player.java
+++ core/src/com/svalero/stellarclash/domain/Player.java
@@ -33,7 +33,29 @@ public class Player extends Character{
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
             move(0,-20);
-
         }
+
+        // Controlamos que el jugador se salga de la pantalla
+        float playerWidth = rect.getWidth();
+        float playerHeight = rect.getHeight();
+
+        // Limitar la posición en X
+        if (position.x < 0) {
+            position.x = 0;
+        }
+        if (position.x > Gdx.graphics.getWidth() - playerWidth) {
+            position.x = Gdx.graphics.getWidth() - playerWidth;
+        }
+        // Limitar la posición en Y
+        if (position.y < 0) {
+            position.y = 0;
+        }
+        if (position.y > Gdx.graphics.getHeight() - playerHeight) {
+            position.y = Gdx.graphics.getHeight() - playerHeight;
+        }
+        //No olvidarse rect, que si no fallan las colisiones
+        rect.setPosition(position.x, position.y);
+
     }
+
 }
