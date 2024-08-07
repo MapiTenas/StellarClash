6cf1163039fe291d67263127c6b93b0435ee9f14
Mapi <mapitenar@hotmail.com>
2024-08-07 11:42:44 +0200

    El player puede moverse + game screen separada

--- core/src/com/svalero/stellarclash/screen/GameScreen.java
+++ core/src/com/svalero/stellarclash/screen/GameScreen.java
@@ -1,16 +1,29 @@
 package com.svalero.stellarclash.screen;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.svalero.stellarclash.domain.Player;
 
 public class GameScreen implements Screen {
+    SpriteBatch batch;
+    Player player;
+
+
     @Override
     public void show() {
-
+        batch = new SpriteBatch();
+        player = new Player(new Texture("badlogic.jpg"));
     }
 
     @Override
     public void render(float delta) {
-
+        ScreenUtils.clear(1, 0, 0, 1);
+        player.manageInput();
+        batch.begin();
+        player.draw(batch);
+        batch.end();
     }
 
     @Override
@@ -35,6 +48,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        player.dispose();
     }
 }
